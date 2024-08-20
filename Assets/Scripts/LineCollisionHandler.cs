0a09b6a40ded75657d9f548e16e10ecc57ee2092
KRONKXD <112854288+KRONKXD@users.noreply.github.com>
2024-08-20 13:30:05 +0300

    Line Collider Fixes

---
+++ Assets/Scripts/LineCollisionHandler.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class LineCollisionHandler : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision detected with: " + collision.gameObject.name);
+        // Handle collision
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("Continuing collision with: " + collision.gameObject.name);
+        // Handle ongoing collision
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("Collision ended with: " + collision.gameObject.name);
+        // Handle collision end
+    }
+}
