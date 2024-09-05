57d551fa251269721f2282f397ec67a88fb1f05d
Eligijus <e.kiudys@gmail.com>
2024-09-05 10:53:09 +0300

    Poses works very rufly

--- Assets/Scripts/HandPose.cs
+++ Assets/Scripts/HandPose.cs
@@ -3,18 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using UnityEditor.SceneManagement;
+using UnityEngine.XR.Hands;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class HandPose : MonoBehaviour
 {
-    
+    public HandType handType;
+    public XRHandSkeletonDriver handSkeletonDriver;
     public GameObject[] fingerObjects;
-    public HandData originlaPoseData;
-    public HandData poseData;
-    
+    public HandData originlaPoseDataLeft;
+    public HandData poseDataLeft;
+    public HandData originlaPoseDataRight;
+    public HandData poseDataRight;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (TryGetComponent(typeof(XRGrabInteractable), out Component grabInteractable))
+        {
+            XRGrabInteractable grabInteractableComponent = (XRGrabInteractable) grabInteractable;
+            grabInteractableComponent.selectEntered.AddListener(SetPose);
+            grabInteractableComponent.selectExited.AddListener(ResetPose);
+        }
+
+
     }
 
     // Update is called once per frame
@@ -22,9 +33,22 @@ public class HandPose : MonoBehaviour
     {
         
     }
-
+    
     public void CreatePose()
     {
+        if (handType == HandType.Left)
+        {
+            CreatePose(ref poseDataLeft, ref originlaPoseDataLeft);
+        }
+        else
+        {
+            CreatePose(ref poseDataRight, ref originlaPoseDataRight);
+        }
+    }
+
+    private void CreatePose(ref HandData poseData, ref HandData originalPoseData)
+    {
+
         if (poseData == null)
         {
             poseData = new HandData();
@@ -41,47 +65,110 @@ public class HandPose : MonoBehaviour
 
         for (int i = 0; i < fingerObjects.Length; i++)
         {
-            poseData.fingerPositions[i] = fingerObjects[i].transform.position;
-            poseData.fingerRotations[i] = fingerObjects[i].transform.rotation;
-            poseData.fingerScales[i] = fingerObjects[i].transform.lossyScale;
+            poseData.fingerPositions[i] = fingerObjects[i].transform.localPosition;
+            poseData.fingerRotations[i] = fingerObjects[i].transform.localRotation;
+            poseData.fingerScales[i] = fingerObjects[i].transform.localScale;
+        }
+    }
+
+    public void UsePose(HandPose handPose)
+    {
+        if (handType == HandType.Left)
+        {
+            UsePose(handPose, ref poseDataLeft, ref originlaPoseDataLeft);
+        }
+        else
+        {
+            UsePose(handPose, ref poseDataRight, ref originlaPoseDataRight);
         }
     }
     
-    public void UsePose(HandPose handPose)
+    private void UsePose(HandPose handPose, ref HandData poseData, ref HandData originalPoseData)
     {
+        
         if (poseData == null)
         {
             Debug.LogError("No Pose Data");
             return;
         }
+
+        originalPoseData = new HandData();
+        originalPoseData.fingerPositions = new Vector3[handPose.fingerObjects.Length];
+        originalPoseData.fingerRotations = new Quaternion[handPose.fingerObjects.Length];
+        originalPoseData.fingerScales = new Vector3[handPose.fingerObjects.Length];
         
         for (int i = 0; i < handPose.fingerObjects.Length; i++)
         {
-            originlaPoseData.fingerPositions[i] = handPose.fingerObjects[i].transform.position;
-            originlaPoseData.fingerRotations[i] = handPose.fingerObjects[i].transform.rotation;
-            originlaPoseData.fingerScales[i] = handPose.fingerObjects[i].transform.lossyScale;
+            originalPoseData.fingerPositions[i] = handPose.fingerObjects[i].transform.localPosition;
+            originalPoseData.fingerRotations[i] = handPose.fingerObjects[i].transform.localRotation;
+            originalPoseData.fingerScales[i] = handPose.fingerObjects[i].transform.localScale;
         }
         
-        for (int i = 0; i < fingerObjects.Length; i++)
+        for (int i = 0; i < handPose.fingerObjects.Length; i++)
         {
-            handPose.fingerObjects[i].transform.position = poseData.fingerPositions[i];
-            handPose.fingerObjects[i].transform.rotation = poseData.fingerRotations[i];
+            handPose.fingerObjects[i].transform.localPosition = poseData.fingerPositions[i];
+            handPose.fingerObjects[i].transform.localRotation = poseData.fingerRotations[i];
             handPose.fingerObjects[i].transform.localScale = poseData.fingerScales[i];
         }
     }
-    
-    public void ResetPose(HandPose handPose)
+
+    public void SetPose(BaseInteractionEventArgs args)
     {
-        if (originlaPoseData == null)
+        Debug.Log("HAHAHAHHAH");
+        if(args.interactorObject is XRDirectInteractor)
         {
-            Debug.LogError("No Original Pose Data");
-            return;
+            Debug.Log("HuHuHuHuHuH");
+            XRDirectInteractor interactor = (XRDirectInteractor) args.interactorObject;
+            HandPose handComponent = interactor.transform.parent.GetComponent<HandPose>();
+            handComponent.handSkeletonDriver.UnsubscribeFromFingerTrackingEvents();
+            UsePose(handComponent);
+        } 
+    }
+    
+    public void ResetPose(BaseInteractionEventArgs args)
+    {
+        if(args.interactorObject is XRDirectInteractor)
+        {
+            XRDirectInteractor interactor = (XRDirectInteractor) args.interactorObject;
+            HandPose handComponent = interactor.transform.parent.GetComponent<HandPose>();
+            ResetHand(handComponent);
+            handComponent.handSkeletonDriver.SubscribeToFingerTrackingEvents();
+        } 
+    }
+
+    public void ResetHand(HandPose handPose)
+    {
+        if (handPose.handType == HandType.Left)
+        {
+
+            if (originlaPoseDataLeft == null)
+            {
+                Debug.LogError("No Original Pose Data");
+                return;
+            }
+
+            for (int i = 0; i < fingerObjects.Length; i++)
+            {
+                handPose.fingerObjects[i].transform.localPosition = originlaPoseDataLeft.fingerPositions[i];
+                handPose.fingerObjects[i].transform.localRotation = originlaPoseDataLeft.fingerRotations[i];
+                handPose.fingerObjects[i].transform.localScale = originlaPoseDataLeft.fingerScales[i];
+            }
         }
-        for (int i = 0; i < fingerObjects.Length; i++)
+        else
         {
-            handPose.fingerObjects[i].transform.position = originlaPoseData.fingerPositions[i];
-            handPose.fingerObjects[i].transform.rotation = originlaPoseData.fingerRotations[i];
-            handPose.fingerObjects[i].transform.localScale = originlaPoseData.fingerScales[i];
+            if (originlaPoseDataRight == null)
+            {
+                Debug.LogError("No Original Pose Data");
+                return;
+            }
+
+            for (int i = 0; i < fingerObjects.Length; i++)
+            {
+                handPose.fingerObjects[i].transform.localPosition = originlaPoseDataRight.fingerPositions[i];
+                handPose.fingerObjects[i].transform.localRotation = originlaPoseDataRight.fingerRotations[i];
+                handPose.fingerObjects[i].transform.localScale = originlaPoseDataRight.fingerScales[i];
+            }
+        
         }
     }
 
@@ -95,8 +182,15 @@ public class HandPoseEditor : Editor
         HandPose hand = (HandPose)target;
         if (GUILayout.Button("Test"))
         {
+          
             hand.CreatePose();
             EditorSceneManager.MarkSceneDirty(EditorSceneManager.GetActiveScene());
         }
     }
+}
+
+public enum HandType
+{
+    Left,
+    Right
 }
