dee23b2c41ffe676c690af2c916e98a80586340b
EligijusK <e.kiudys@gmail.com>
2024-09-11 21:13:59 +0300

    Working all dors

--- Assets/Scripts/OpenDoors.cs
+++ Assets/Scripts/OpenDoors.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 public class OpenDoors : MonoBehaviour
 {
     [SerializeField] Vector3 addForce;
@@ -20,6 +24,29 @@ public class OpenDoors : MonoBehaviour
     
     public void AddForce()
     {
+        rb.constraints = RigidbodyConstraints.None;
         rb.AddForce(addForce, ForceMode.Impulse);
     }
 }
+
+#if UNITY_EDITOR
+
+[CustomEditor(typeof(OpenDoors))]
+public class OpenDoorsEditor : Editor
+{
+    
+    
+    
+    public override void OnInspectorGUI() {
+        OpenDoors doors = (OpenDoors)target;
+        
+        base.OnInspectorGUI();
+        
+        if (GUILayout.Button("Test"))
+        {
+            doors.AddForce();
+        }
+    }
+}
+
+#endif
